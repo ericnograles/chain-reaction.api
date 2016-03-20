c5bb311e423ac764a20b320adeefc39856a5ce69
Eric Nograles <ENograles@352inc.com>
2016-03-20 10:41:48 -0400

    All sorts of stuff.  Webpack config fixes to go to chain-reaction.common source folder.  Updated lib with the latest API shenanigans.  References are now "in development mode" for the web and references the chain-reaction.common folder directly so live reloads work.

---
+++ api/controllers/TwitterController.js
@@ -0,0 +1,11 @@
+/**
+ * TwitterController
+ *
+ * @description :: Server-side logic for managing Twitters
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
