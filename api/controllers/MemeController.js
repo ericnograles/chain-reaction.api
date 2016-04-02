35821cceb565cc389815aa0a2bd15924168385d7
Eric Nograles <ENograles@352inc.com>
2016-04-02 07:09:30 -0400

    Non-meme baseline

--- api/controllers/MemeController.js
+++ api/controllers/MemeController.js
@@ -11,11 +11,11 @@ module.exports = {
 
 function find(req, res) {
   var memeUrls = [
-    'http://cdn.meme.am/instances/500x/13734123.jpg',
-    'http://s2.quickmeme.com/img/6d/6d68ad25beab779467cf2e95c7c3de742f4df17ad4e38176a71886f566816424.jpg',
-    'http://cdn.pophangover.com/wp-content/uploads/2013/12/mem2.png',
-    'http://memesvault.com/wp-content/uploads/Best-Meme-03.jpg',
-    'http://memesvault.com/wp-content/uploads/I-Have-No-Idea-What-Im-Doing-Dog-02.jpg'
+    'http://localhost:1337/images/meme-1.jpg',
+    'http://localhost:1337/images/meme-2.jpg',
+    'http://localhost:1337/images/meme-3.png',
+    'http://localhost:1337/images/meme-4.jpg',
+    'http://localhost:1337/images/meme-5.jpg',
   ];
 
   return res.json({memes: memeUrls});
